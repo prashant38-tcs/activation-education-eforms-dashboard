c785b017cd645802724e059f031f332e4e9924d8
prashant38-tcs <prashant.38@tcs.com>
2026-09-05 21:48:36 +0530

    Update UserRepository.java

--- src/main/java/com/tcsion/eforms/repository/UserRepository.java
+++ src/main/java/com/tcsion/eforms/repository/UserRepository.java
@@ -1,13 +1,16 @@
 package com.tcsion.eforms.repository;
+
 import com.tcsion.eforms.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
     Optional<User> findByUsernameIgnoreCase(String username);
     Optional<User> findByEmailIgnoreCase(String email);
+    Optional<User> findByFullNameIgnoreCase(String fullName);
     boolean existsByUsernameIgnoreCase(String username);
     List<User> findByActiveTrue();
     List<User> findByRoles_RoleCodeAndActiveTrue(String roleCode);
